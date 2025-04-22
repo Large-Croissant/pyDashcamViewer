cb2c6bf851677b98b71827bdf2d3a1ddda617832
Joey <jpg2368@rit.edu>
2025-04-22 19:14:18 -0400

    created setup check and auto download exiftool, added logging

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import logging
 DIR = pathlib.Path(__file__).parent
 EXIFTOOL_PATH = DIR / "exiftool" / "exiftool.exe"
 
+logging.basicConfig(filename=DIR / "log.log", format="%(asctime)s [%(levelname)s]: %(message)s")
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
 
 def cli(args: list[str]): ...
 
@@ -21,14 +25,21 @@ def main(args: list[str]):
 
 
 def setup():
+    logger.info("Beginning setup check")
     if EXIFTOOL_PATH.exists():
+        logger.info("Exiftool found")
         return
-
-    EXIFTOOL_VERSION_FILE = "exiftool-13.27_64.zip"
-    os.system(rf"curl https://exiftool.org/{EXIFTOOL_VERSION_FILE} -o {DIR / 'exiftool.zip'}")
+    logger.warning("Exiftool not found, will download")
+    EXIFTOOL_VERSION = "exiftool-13.27_64"
+    os.system(rf"curl https://exiftool.org/{EXIFTOOL_VERSION}.zip -o {DIR / 'exiftool.zip'}")
+    logger.info("Exiftool downloaded")
     os.system(rf"tar -xf {DIR / 'exiftool.zip'}")
-    os.rename(DIR / EXIFTOOL_VERSION_FILE, DIR / "exiftool")
-    os.rename(DIR / "exiftool" / "exiftool(-k).exe", DIR / "exiftool" / "exiftool.exe")
+    logger.info("Extracted zip archive")
+    os.rename(DIR / EXIFTOOL_VERSION, DIR / "exiftool")
+    os.rename((DIR / "exiftool" / "exiftool(-k).exe"), DIR / "exiftool" / "exiftool.exe")
+    logger.info("Files renamed")
+    os.remove(DIR / "exiftool.zip")
+    logger.info("Cleaned up zip file")
 
 
 if __name__ == "__main__":
