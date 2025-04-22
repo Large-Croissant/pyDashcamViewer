054dcdf41e0cb9db18c8dafdfc7cf9014c2a41f0
Joey <jpg2368@rit.edu>
2025-04-22 18:55:32 -0400

    added exiftool setup stuff

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sys
 import pathlib
 import os
+import logging
 
 DIR = pathlib.Path(__file__).parent
+EXIFTOOL_PATH = DIR / "exiftool" / "exiftool.exe"
 
 
 def cli(args: list[str]): ...
@@ -18,7 +20,15 @@ def main(args: list[str]):
         cli(args)
 
 
-def setup(): ...
+def setup():
+    if EXIFTOOL_PATH.exists():
+        return
+
+    EXIFTOOL_VERSION_FILE = "exiftool-13.27_64.zip"
+    os.system(rf"curl https://exiftool.org/{EXIFTOOL_VERSION_FILE} -o {DIR / 'exiftool.zip'}")
+    os.system(rf"tar -xf {DIR / 'exiftool.zip'}")
+    os.rename(DIR / EXIFTOOL_VERSION_FILE, DIR / "exiftool")
+    os.rename(DIR / "exiftool" / "exiftool(-k).exe", DIR / "exiftool" / "exiftool.exe")
 
 
 if __name__ == "__main__":
